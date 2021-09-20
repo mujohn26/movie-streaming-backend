b88252730329c3713d355fd56401f7ff867ea429
Nelson Bassey <nellybaz10@gmail.com>
2021-09-20 11:04:50 +0200

    registration with no credentials

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+
+  def create 
+    render json: {error:"email and password required"}, status: 400
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,10 +1,14 @@
 # require_relative "../../app/controllers/users_controller"
+require "rails_helper"
 
-RSpec.describe UsersController, type: :contorller do
+RSpec.describe UsersController, type: :controller do
 
   describe '#POST create' do
-    it 'registers with empty fields' do
-      
+    it 'registers user with empty fields' do
+      post :create, params: {}
+
+      expect(response).to have_http_status(400)
+      expect(response.parsed_body['error']).to eq "email and password required"
     end
-end
+  end
 end
