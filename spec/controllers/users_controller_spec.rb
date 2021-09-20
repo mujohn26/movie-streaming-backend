a6ba20e483a181d857cf58ca4e8c5003611f563e
Nelson Bassey <nellybaz10@gmail.com>
2021-09-20 10:22:13 +0200

    initial project setup

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,10 @@
+# require_relative "../../app/controllers/users_controller"
+
+RSpec.describe UsersController, type: :contorller do
+
+  describe '#POST create' do
+    it 'registers with empty fields' do
+      
+    end
+end
+end
